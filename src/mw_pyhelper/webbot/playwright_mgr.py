2af0167ad099ad060f1a3631745985491b1d3973
Michael Wan <hkmicmic@gmail.com>
2024-10-05 21:14:22 +0100

    fix indent 3/4 spaces problem

--- src/mw_pyhelper/webbot/playwright_mgr.py
+++ src/mw_pyhelper/webbot/playwright_mgr.py
@@ -8,29 +8,29 @@ from ..cfgloader import AppCfg
 from . import constants
 
 def get_browser(appcfg: AppCfg, playwright: Playwright) -> Optional[Browser]:
-   if playwright is None:
-      mylogger.debug('No playwright')
-      return None
-   
-   mylogger = logging.getLogger(__name__)
+    if playwright is None:
+        mylogger.debug('No playwright')
+        return None
 
-   is_headless = False
-   if appcfg.get(constants.CFG_KEY_HEADLESS_MODE) and appcfg.get(constants.CFG_KEY_HEADLESS_MODE).lower() == 'true':
-      is_headless = True
+    mylogger = logging.getLogger(__name__)
 
-   cfg_value = appcfg.get(constants.CFG_KEY_BROWSER_TYPE)
-   if cfg_value and cfg_value.lower() == 'chrome':
-      mylogger.debug('Chrome type browser return')
-      return playwright.chromium.launch(headless=is_headless)
-   elif cfg_value and cfg_value.lower() == 'firefox':
-      mylogger.debug('Firefox type browser return')
-      return playwright.firefox.launch(headless=is_headless)
-   elif cfg_value and cfg_value.lower() == 'webkit':
-      mylogger.debug('Webkit type browser return')
-      return playwright.webkit.launch(headless=is_headless)
-   
-   mylogger.debug('No playwright return')
-   return None
+    is_headless = False
+    if appcfg.get(constants.CFG_KEY_HEADLESS_MODE) and appcfg.get(constants.CFG_KEY_HEADLESS_MODE).lower() == 'true':
+        is_headless = True
+
+    cfg_value = appcfg.get(constants.CFG_KEY_BROWSER_TYPE)
+    if cfg_value and cfg_value.lower() == 'chrome':
+        mylogger.debug('Chrome type browser return')
+        return playwright.chromium.launch(headless=is_headless)
+    elif cfg_value and cfg_value.lower() == 'firefox':
+        mylogger.debug('Firefox type browser return')
+        return playwright.firefox.launch(headless=is_headless)
+    elif cfg_value and cfg_value.lower() == 'webkit':
+        mylogger.debug('Webkit type browser return')
+        return playwright.webkit.launch(headless=is_headless)
+
+    mylogger.debug('No playwright return')
+    return None
 
 def navigate_to(page: Page, explicit_waittime: int, page_url: str, match_title: Union[Pattern[str], str], retry_count: int = 0) -> bool:
     mylogger = logging.getLogger(__name__)
@@ -56,7 +56,6 @@ def check_url(page: Page, explicit_waittime: int, pattern: Union[Pattern[str], s
     mylogger = logging.getLogger(__name__)
     try:
         expect(page).to_have_url(pattern, timeout=explicit_waittime * 1000)
-
         return True
     except AssertionError:
         mylogger.error(f'Current URL does not match with "{pattern}":"{page.url}"')
